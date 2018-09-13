f1f168289bab4f7008c432f5d45f43cf619477ed
Xinke Chen <xinkecf35@gmail.com>
2018-09-12 22:31:48 -0400

    Adjusted issue with eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'google'
   ],
   rules: {
+    'indent': ['error', 2],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
