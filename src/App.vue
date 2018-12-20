baff795ec7757a377b9c319da456cd1d4b0c191c
Xinke Chen <xinkecf35@gmail.com>
2018-12-19 19:40:57 -0500

    Customized styling for navbar
    This commit styles the nav bar in the App view as long with adding inverted color version of logo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
     <div id="nav" v-if="['login', 'signup'].indexOf($route.name) === -1">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Log In</router-link> |
-      <router-link to="/signup">Sign Up</router-link>
+      <img id="nav-logo" alt="transfr logo" src="./assets/logo-invert.svg">
+      <div id="nav-links">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/login">Log In</router-link>
+        <router-link to="/signup">Sign Up</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
@@ -21,6 +24,7 @@
 export default {
   beforeCreate() {
     document.body.classList.add('home');
+    document.body.classList.add('no-margin');
   },
 };
 </script>
@@ -34,12 +38,36 @@ export default {
   color: $primarycolor;
 }
 #nav {
-  padding: 30px;
+  background-color: $secondarycolor;
+  justify-content: flex-start;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
   a {
     font-weight: bold;
     color: $primarycolor;
-    &.router-link-exact-active {
-      color: $accentcolor;
+  }
+}
+#nav-logo{
+  flex-grow: 1;
+  order: 1;
+  height: 3em;
+}
+#nav-links {
+  justify-content: flex-end;
+  display: flex;
+  flex-direction: row-wrap;
+  flex-grow: 2;
+  order: 2;
+  margin-right: 2em;
+  a {
+    font-size: 1.1em;
+    color: $backgroundcolor;
+    text-decoration: none;
+    display: block;
+    padding: 1em 2em 1em 2em;
+    &:hover {
+      background-color: $primarycolor;
     }
   }
 }
