126d4204a484aa00106bba9ef0f50cbb13178d71
Xinke Chen <xinkecf35@gmail.com>
2019-05-19 19:27:39 -0400

    Started work on public VCard Access
    This commit starts work on converting JSON data into VCard format. Currently able to request data via XHR and output formatted data to console.

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="Error">
+    <h1>
+      We're sorry, there is nothing here.
+      We aplogize for the inconvenience.
+    </h1>
+  </div>
+</template>
+
---
+++ src/views/PublicCard.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="card">
+    <div id="card-info" class="level-1">
+
+    </div>
+  </div>
+</template>
+<script>
+import {ajaxRequest} from '@/functions';
+
+// Array of attributes that should be handled specially
+const complexAttributes = ['address', 'email', 'telephone'];
+
+const VCardFieldMap = {
+  address: 'ADR',
+  birthday: 'BDAY',
+  email: 'EMAIL',
+  fullName: 'FN',
+  impp: 'IMPP',
+  name: 'N',
+  nickname: 'NICKNAME',
+  note: 'NOTE',
+  organization: 'ORG',
+  telephone: 'TEL',
+};
+
+export default {
+  name: 'publicCard',
+  beforeRouteEnter(to, from, next) {
+    // Fetch Data if appropriate
+    const cardId = to.params.card;
+    let cardDataURL = 'https://api.transfr.info/v1/card/' + cardId;
+    if (process.env.NODE_ENV === 'development') {
+      cardDataURL = 'https://api.transfr.test/v1/card/' + cardId;
+    }
+    let cardDataPromise = ajaxRequest('GET', cardDataURL);
+    cardDataPromise.then(function(response) {
+      next((vm) => vm.convertJSONtoVCard(response.card));
+    }).catch(function(err) {
+      if (err.status === 404) {
+        next((vm) => vm.notFound = true);
+      }
+      next(err);
+    });
+  },
+  methods: {
+    createVCard: function() {
+    },
+    convertJSONtoVCard: function(cardJSON) {
+      // Prefix data with VCard identifier and Product ID
+      let vCardData = 'BEGIN:VCARD\r\nVERSION:3.0\r\n';
+      vCardData += 'PRODID:-//transfr.info//Web Client\r\n';
+      // Process JSON attributes into VCard format
+      let JSONFields = Object.keys(cardJSON);
+      JSONFields = JSONFields.filter((key) => key !== 'description');
+      const simpleKeys =
+        JSONFields.filter((key) => complexAttributes.indexOf(key) === -1);
+      const multipleKeys =
+        JSONFields.filter((key) => complexAttributes.indexOf(key) > -1);
+      simpleKeys.forEach(function(key) {
+        const field = VCardFieldMap[key];
+        vCardData += field + ':' + cardJSON[key] + '\r\n';
+      });
+      multipleKeys.forEach(function(key) {
+        const field = VCardFieldMap[key];
+        const values = cardJSON[key];
+        values.forEach(function(value) {
+          const formattedString =
+            ['TYPE=', value.type, ':', value.value].join('');
+          vCardData += field + ';' + formattedString + '\r\n';
+        });
+      });
+      // End Data with VCard terminator
+      vCardData += 'END:VCARD\r\n';
+      console.log(vCardData);
+      return vCardData;
+    },
+  },
+  props: {
+    notFound: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+  #card {
+    background-color: $secondarycolor;
+  }
+  #card-info {
+    position: absolute;
+    border-radius: 12px;
+    left:50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    min-height: 10rem;
+    @media #{$breakpoint-sm} {
+        width: 95%;
+    }
+    @media #{$breakpoint-md} {
+        width: 55%;
+    }
+    @media #{$breakpoint-lg} {
+        width: 40%;
+    }
+  }
+
+</style>
